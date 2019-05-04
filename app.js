b15fe0327c5631566bf8c38b3ec848fc340a1538
Aiden Berzins <bberzins@student.cccs.edu>
2019-05-04 10:26:08 -0600

    Added v-bind and v-text directive examples.
    Continuing to add examples of vue directives.

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const mainContent = new Vue({
   el: '#app',
   data: {
-    h1: 'This is the H1 Element',
-    p1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    h2: 'This is the H2 Element',
-    p2: 'This is the p element',
+    title: 'This is the H1 Element',
+    message: 'This is an example of V-Text directive. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    title2: 'This is the H2 Element',
+  }
+})
+const secondaryContent = new Vue({
+  el: '#app2',
+  data: {
+    title: 'This is an example of the v-bind directive on an image',
+    img:{
+      src: 'http://placekitten.com/800/500',
+      alt: 'The alt text is dynamically added through Vue.',
+    }
   }
 })
